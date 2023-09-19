3d45b707c66570b85e4fece6a49fc9e903a76ad9
Elena Klepova <elena.klyopova@gmail.com>
2023-09-19 21:17:39 +0300

    Homework Lesson №8 TestData

--- src/test/java/utils/RandomUtils.java
+++ src/test/java/utils/RandomUtils.java
@@ -51,7 +51,7 @@ public class RandomUtils {
     }
 
     public String getRandomPicture() {
-        return faker.options().option("testpicture.jpg");
+        return faker.options().option("test.jpg");
     }
 
     public String getRandomState() {
